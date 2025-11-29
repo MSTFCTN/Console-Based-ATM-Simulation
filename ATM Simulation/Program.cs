14185f71f3b89a236ab44be27f2840b93147d52b
Mustafa Eren Çetin <mustafaerenofficial@gmail.com>
2025-11-29 22:03:04 +0300

    I added json file that storing account data

--- ATM Simulation/Program.cs
+++ ATM Simulation/Program.cs
@@ -9,21 +9,67 @@ using System.IO;
 namespace GeminiCS {
     class App {
         public static void Main() {
+            JSON_Operations.read_from_JSON();
+            Console.WriteLine("Dosya Yolu: " + Path.GetFullPath("Accounts.json"));
+            Console.WriteLine();
+            Show_Accounts();
+
             bool döngü = true;
             ConsoleKeyInfo keyInfo;
             while (döngü) {
                 Account Account = new Account(App.Set_Username(), App.Set_Password());
-                Console.WriteLine(")=================(");
-                Console.WriteLine($"Username = {Account._Username}");
-                Console.WriteLine($"Password = {Account._Password}");
-                Console.WriteLine($"Balance = {Account._Balance} $");
-                Console.WriteLine(")=================(");
+
+                Console.Clear();
+                
+                Console.WriteLine($"Username = {Account.Username}");
+                Console.WriteLine($"Password = {Account.Password}");
+                Console.WriteLine($"Balance = {Account.Balance} $");
+                
+                Accounts.Add(Account);
+                JSON_Operations.write_to_JSON();
+                
+                Console.WriteLine("\rPress ESC to exit and see previous created accounts!");
+                
                 keyInfo = Console.ReadKey(true);
                 if (keyInfo.Key == ConsoleKey.Escape) döngü = false;
+                
+                Console.Clear();
             }
+
+
 
             Console.ReadKey();
         }
+
+        public static List<Account> Accounts = new List<Account>();
+
+
+
+
+
+        // burda hata var
+
+
+
+        public static void Show_Accounts() {
+           // Account account = new Account();
+            if (account.Username == "0") {
+                foreach (Account show_accounts in Accounts) {
+                    Console.WriteLine(")=================(");
+                    Console.WriteLine($"Username = {show_accounts.Username}");
+                    Console.WriteLine($"Password = {show_accounts.Password}");
+                    Console.WriteLine($"Balance = {show_accounts.Balance} $");
+                }
+                //Console.WriteLine(")=================(");
+            }
+            
+        }
+
+
+
+
+
+
 
         public static string Set_Username() {
             Console.Write("Username: ");
@@ -69,30 +115,37 @@ namespace GeminiCS {
         
     }
 
+    class JSON_Operations {
+        public static void write_to_JSON() {
+            var settings = new JsonSerializerOptions { WriteIndented = true };
+            string jsonText = JsonSerializer.Serialize(App.Accounts, settings);
+
+            File.WriteAllText("Accounts.json", jsonText);
+        }
+
+        public static void read_from_JSON() {
+            if (!File.Exists("Accounts.json")) {
+                return;
+            }
+
+            string jsonText = File.ReadAllText("Accounts.json");
+            if (!string.IsNullOrEmpty(jsonText)) {
+                App.Accounts = JsonSerializer.Deserialize<List<Account>>(jsonText);
+                Console.WriteLine("Old Records Uploaded!");
+            }
+        }
+    }
+
     class Account {
-        private string m_USERNAME;
-        private string m_PASSWORD;
-        private int m_BALANCE;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Balance { get; set; }
 
         public Account(string username, string password) {
             Random rnd = new Random();
-            m_BALANCE = rnd.Next(1000, 5001);
-            m_USERNAME = username;
-            m_PASSWORD = password;
-        }
-
-        public string _Username {
-            get { return m_USERNAME; }
-            set { m_USERNAME = value; }
-        }
-
-        public string _Password {
-            get { return m_PASSWORD; }
-            set { m_PASSWORD = value; }
-        }
-
-        public int _Balance {
-            get { return m_BALANCE; }
+            Balance = rnd.Next(1000, 5001);
+            Username = username;
+            Password = password;
         }
     }
 }
