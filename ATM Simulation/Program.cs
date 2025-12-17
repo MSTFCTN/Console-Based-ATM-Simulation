747ad42c0cf3e697487aee9a02c28b57b0bc52ad
Mustafa Eren Çetin <mustafaerenofficial@gmail.com>
2025-12-17 22:40:56 +0300

    Some menu mechanics and little changes

--- ATM Simulation/Program.cs
+++ ATM Simulation/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.Text;
@@ -11,47 +13,149 @@ using System.Threading;
 
 namespace GeminiCS {
     class App {
-        public static void Main() {
-            JSON_Operations.read_from_JSON();
+        public static List<Account> Accounts = new List<Account>();
+        public static Account Account;
 
-            bool döngü = true;
+        public static void Main() {
+            Console.CursorVisible = false;
+            
+            JSON_Operations.read_from_JSON();
+            //Account Account = new Account(App.Set_Username(), App.Set_Password());
+            //Accounts.Add(Account);
+            //JSON_Operations.write_to_JSON();
+
             ConsoleKeyInfo keyInfo;
 
-            Show_Accounts();
-            while (döngü) {
-                Account Account = new Account(App.Set_Username(), App.Set_Password());
+            bool program = true;
+            while (program) {
+                int main_select;
+                main_select = Menu.MainMenu();
 
-                Console.Clear();
-                Console.WriteLine($"Balance = {Account.Balance} $");
+                if(main_select == 0) {
+                    int login_select;
+                    bool login;
+                    while (!(login = Login())) {
+                        Console.WriteLine("Wrong username or password!");
+                        Console.WriteLine("Press any key to retry or press ESC to return main menu");
+                        keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Escape) break;
+                    }
 
-                Accounts.Add(Account);
-                JSON_Operations.write_to_JSON();
+                    while (login) {
+                        Console.Clear();
+                        Console.WriteLine($"Welcome {Account.Username} !");
+                        Console.WriteLine($"Your Balance : {Account.Balance}");
+                        Console.WriteLine();
 
-                Console.WriteLine();
-                Console.WriteLine("Press ESC to exit or press any key to continue");
-                
-                keyInfo = Console.ReadKey(true);
-                if (keyInfo.Key == ConsoleKey.Escape) döngü = false;
-                
-                Console.Clear();
-            }
-            Console.ReadKey();
-        }
+                        login_select = Menu.AccountMenu();
 
-        public static List<Account> Accounts = new List<Account>();
+                        // Deposit Money
+                        while (login_select == 0) {
+                            Console.Clear();
+                            Console.WriteLine("Please place your cash neatly into the slot!");
+                            Console.WriteLine("Press ENTER when you done or press ESC to return account menu!");
+                            keyInfo = Console.ReadKey(true);
+                            if (keyInfo.Key == ConsoleKey.Enter) {
+                                Console.Clear();
+                                Console.Write("Your money is counting");
+                                Thread.Sleep(1000);
+                                Console.Write(".");
+                                Thread.Sleep(1000);
+                                Console.Write(".");
+                                Thread.Sleep(1000);
+                                Console.Write(".");
+                                Thread.Sleep(1000);
 
-        public static void Show_Accounts() {             
-            foreach (Account show_accounts in Accounts) {
-                if (show_accounts.Username != "0" || show_accounts.Password != "0") {
-                    Console.WriteLine("\r\r");
-                    Console.WriteLine(")=================(");
-                    Console.WriteLine($"Username = {show_accounts.Username}");
-                    Console.WriteLine($"Password = {show_accounts.Password}");
-                    Console.WriteLine($"Balance = {show_accounts.Balance} $");
-                    Console.WriteLine(")=================(");
-                    Console.WriteLine("\r\r");
+                                Random rnd = new Random();
+                                int add_balance;
+                                add_balance = rnd.Next(1, 1001);
+
+                                Console.Clear();
+                                Account.Balance += add_balance;
+                                JSON_Operations.write_to_JSON();
+                                Console.WriteLine($"{add_balance}$ succesfully added to your balance");
+                                Console.WriteLine("Thank you for choosing us <3");
+                                Thread.Sleep(1000);
+                                break;
+                            }
+                            else if (keyInfo.Key == ConsoleKey.Escape) break;
+                        }
+
+
+                        // Withdraw Money
+                        while (login_select == 1) {
+                            Console.Clear();
+                            Console.WriteLine("(Enter 0 to back account menu!)");
+                            Console.Write("Please enter amount : ");
+
+                            string input = Console.ReadLine();
+                            int amount;
+                            while(!int.TryParse(input ,out amount) || amount > Account.Balance || amount < 0){
+                                Console.Clear();
+                                Console.WriteLine("Please enter a valid amount!");
+                                Console.Write(": ");
+                                input = Console.ReadLine();
+                            }
+
+                            if (amount == 0) break;
+
+                            Console.Clear();
+                            Console.Write("Your money is preparing");
+                            Thread.Sleep(1000);
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+                            Console.Write(".");
+                            Thread.Sleep(1000);
+
+                            Console.Clear();
+                            Console.WriteLine("Please take your money in the slot!");
+                            Console.WriteLine("Press ENTER when you done");
+                            keyInfo = Console.ReadKey();
+
+                            while(keyInfo.Key != ConsoleKey.Enter) {
+                                keyInfo = Console.ReadKey();
+                            }
+
+                            Console.Clear();
+                            Account.Balance -= amount;
+                            JSON_Operations.write_to_JSON();
+                            Console.WriteLine("Thank you for choosing us <3");
+                            Thread.Sleep(1000);
+                            break;
+                        }
+
+                        if (login_select == 2) break;
+                    }
                 }
             }
+        }
+
+        
+        public static bool Login() {
+            string username = null;
+            string password = null;
+            int login = 0;
+
+            Console.Clear();
+
+            Console.Write("Username: ");
+            while (string.IsNullOrEmpty(username = Console.ReadLine())) {
+                Console.Clear();
+                Console.Write("Username: ");
+            }
+
+            password = Set_Password();
+
+            foreach(Account acc in Accounts) {
+                if (acc.Username == username && acc.Password == password) {
+                    Account = acc;
+                    return true;
+                }
+            }
+
+            return false;
         }
 
 
@@ -156,6 +260,101 @@ namespace GeminiCS {
             Username = username;
             Password = password;
         }
+
+        public static void Show_Accounts() {
+            foreach (Account show_accounts in App.Accounts) {
+                if (show_accounts.Username != null || show_accounts.Password != null) {
+                    Console.WriteLine("\r\r");
+                    Console.WriteLine(")=================(");
+                    Console.WriteLine($"Username = {show_accounts.Username}");
+                    Console.WriteLine($"Password = {show_accounts.Password}");
+                    Console.WriteLine($"Balance = {show_accounts.Balance} $");
+                    Console.WriteLine(")=================(");
+                    Console.WriteLine("\r\r");
+                }
+            }
+        }
+    }
+
+    class Menu {
+        public static int MainMenu() {
+
+            ConsoleKeyInfo keyInfo;
+            bool menu = true;
+            int select = 0;
+            int CursorLeft = 28, CursorTop = 0;
+
+            Console.Clear();
+            Console.WriteLine("1 - Login To Your Account");
+            Console.WriteLine("2 - Sign up (Free Balance)");
+            Console.WriteLine("3 - Exit");
+
+            while (menu) {
+                Console.SetCursorPosition(CursorLeft, CursorTop);
+                Console.Write("<<");
+
+                keyInfo = Console.ReadKey(true);
+
+                if ((keyInfo.Key == ConsoleKey.UpArrow || keyInfo.Key == ConsoleKey.W) && select > 0) {
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                    Console.Write("  ");
+                    CursorTop--;
+                    select--;
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+
+                }
+                else if ((keyInfo.Key == ConsoleKey.DownArrow || keyInfo.Key == ConsoleKey.S) && select < 2) {
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                    Console.Write("  ");
+                    CursorTop++;
+                    select++;
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter) break;
+            }
+
+            return select;
+        }
+
+        public static int AccountMenu() {
+
+            ConsoleKeyInfo keyInfo;
+            bool menu = true;
+            int select = 0;
+            int CursorLeft = 19, CursorTop = 3;
+
+            Console.WriteLine("1 - Deposit Money");
+            Console.WriteLine("2 - Withdraw Money");
+            Console.WriteLine("3 - Back");
+
+            while (menu) {
+                Console.SetCursorPosition(CursorLeft, CursorTop);
+                Console.Write("<<");
+
+                keyInfo = Console.ReadKey(true);
+
+                if ((keyInfo.Key == ConsoleKey.UpArrow || keyInfo.Key == ConsoleKey.W) && select > 0) {
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                    Console.Write("  ");
+                    CursorTop--;
+                    select--;
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+
+                }
+                else if ((keyInfo.Key == ConsoleKey.DownArrow || keyInfo.Key == ConsoleKey.S) && select < 2) {
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                    Console.Write("  ");
+                    CursorTop++;
+                    select++;
+                    Console.SetCursorPosition(CursorLeft, CursorTop);
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter) break;
+            }
+
+            return select;
+        }
+
+
     }
 }
 
